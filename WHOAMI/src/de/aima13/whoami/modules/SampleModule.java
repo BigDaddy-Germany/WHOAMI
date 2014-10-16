7e98ab934eef4e24cab07b343c51c44338fb45a8
Marco Dörfler <marco@doerflers.net>
2014-10-16 15:50:39 +0200

    Beispielmodul an neues Interface angepasst

--- WHOAMI/src/de/aima13/whoami/modules/SampleModule.java
+++ WHOAMI/src/de/aima13/whoami/modules/SampleModule.java
@@ -2,28 +2,35 @@ package de.aima13.whoami.modules;
 
 import de.aima13.whoami.Analyzable;
 
+import java.io.File;
+import java.util.List;
+import java.util.SortedMap;
+
 /**
  * Created by D060469 on 16.10.14.
  */
 public class SampleModule implements Analyzable {
     @Override
-    public void getFilter() {
+    public List<String> getFilter() {
+
+        return null;
+    }
+
+    @Override
+    public void setFileInputs(List<File> files) throws Exception {
 
     }
 
     @Override
-    public void setFileInputs() {
+    public String getHtml() {
 
+        return null;
     }
 
     @Override
-    public void getHtml() {
+    public SortedMap<String, String> getCsvContent() {
 
-    }
-
-    @Override
-    public void getCsvContent() {
-
+        return null;
     }
 
     @Override
