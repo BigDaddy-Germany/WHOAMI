5d8cd1dd8225b3fde90b0eab4c71ba0dd129a09d
n2code <n2code@users.noreply.github.com>
2014-10-16 18:56:38 +0200

    SortedList mit List ersetzt
    Listen sind automatisch nach Einfügereihenfolge sortiert und können falls nötig mit Collections.sort manuell sortiert werden. Die vorherige SortedList war aus JavaFX 8, also für uns nicht relevant.
    
    Basisprojekt ist jetzt leer und kompilierbar.

--- WHOAMI/src/de/aima13/whoami/CsvCreator.java
+++ WHOAMI/src/de/aima13/whoami/CsvCreator.java
@@ -1,12 +1,12 @@
 package de.aima13.whoami;
 
-import com.sun.javafx.collections.transformation.SortedList;
+import java.util.List;
 
 /**
  * Created by D060469 on 16.10.14.
  */
 public class CsvCreator {
-    public static void saveCsv(SortedList<Representable> representables) throws Exception {
+    public static void saveCsv(List<Representable> representables) throws Exception {
 
     }
 }
--- WHOAMI/src/de/aima13/whoami/ReportCreator.java
+++ WHOAMI/src/de/aima13/whoami/ReportCreator.java
@@ -1,12 +1,12 @@
 package de.aima13.whoami;
 
-import com.sun.javafx.collections.transformation.SortedList;
+import java.util.List;
 
 /**
  * Created by D060469 on 16.10.14.
  */
 public class ReportCreator {
-    public ReportCreator(SortedList<Representable> representables) {
+    public ReportCreator(List<Representable> representables) {
 
     }
 
