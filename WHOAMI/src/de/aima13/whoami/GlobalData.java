4023a7caf126159e4b8e11b6ebf9125a4ea2fec5
Marco Dörfler <marco@doerflers.net>
2014-10-17 18:54:47 +0200

    GlobalData als Singleton angepasst
    GlobalData sollte als Singleton vorhanden sein, dass sie immer auf-
    rufbar ist.

--- WHOAMI/src/de/aima13/whoami/GlobalData.java
+++ WHOAMI/src/de/aima13/whoami/GlobalData.java
@@ -11,6 +11,9 @@ public class GlobalData implements Representable {
 	 * @todo Global Data muss Thrad-safe sein
 	 */
 
+	// Speicherung der Instanz für Singleton Klasse
+	private static GlobalData instance;
+
 	@Override
 	public String getHtml() {
 
@@ -21,6 +24,20 @@ public class GlobalData implements Representable {
 	public SortedMap<String, String> getCsvContent() {
 
 		return null;
+	}
+
+
+	// Konstruktor privat, Klasse ist Singleton
+	private GlobalData() {
+
+	}
+
+	// getInstance für Singleton-Klasse
+	public GlobalData getInstance() {
+		if (instance == null) {
+			instance = new GlobalData();
+		}
+		return instance;
 	}
 
 	public void proposeData(String key, String value) {
