74b46dd51f43dd632f9583765c55a22ac5060c8e
momoXD007 <momoXD007@users.noreply.github.com>
2014-10-17 13:36:36 +0200

    Anfangscommit
    Essens Modul hinzugrfügt

---
+++ WHOAMI/src/de/aima13/whoami/modules/Food.java
@@ -0,0 +1,40 @@
+package de.aima13.whoami.modules;
+
+import de.aima13.whoami.Analyzable;
+
+import java.io.File;
+import java.util.List;
+import java.util.SortedMap;
+
+/**
+ * Created by D060469 on 16.10.14.
+ */
+public class Food implements Analyzable {
+	@Override
+	public List<String> getFilter() {
+
+		return null;
+	}
+
+	@Override
+	public void setFileInputs(List<File> files) throws Exception {
+
+	}
+
+	@Override
+	public String getHtml() {
+
+		return null;
+	}
+
+	@Override
+	public SortedMap<String, String> getCsvContent() {
+
+		return null;
+	}
+
+	@Override
+	public void run() {
+
+	}
+}
