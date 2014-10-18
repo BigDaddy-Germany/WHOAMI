448bfa4f8710f4b3fd5c0dc8b3cc7b809f2b166e
n2code <n2code@users.noreply.github.com>
2014-10-18 22:20:07 +0200

    Spielefilter und Entgegennehmen der Dateiliste
    Implementiert getFilter() und setFileInputs() im Spielemodul

--- WHOAMI/src/de/aima13/whoami/modules/Games.java
+++ WHOAMI/src/de/aima13/whoami/modules/Games.java
@@ -3,6 +3,7 @@ package de.aima13.whoami.modules;
 import de.aima13.whoami.Analyzable;
 
 import java.io.File;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.SortedMap;
 
@@ -10,15 +11,43 @@ import java.util.SortedMap;
  *
  */
 public class Games implements Analyzable {
+	private List<File> exeFiles;
+	private File steamAppsFolder;
+
+	/**
+	 * Spielemodul fragt nach einer Liste von Executables und benötigt den Pfad eines
+	 * gegebenenfalls vorhandenen "SteamApps"-Ordners
+	 *
+	 * @return Filterliste
+	 */
 	@Override
 	public List<String> getFilter() {
-
-		return null;
+		List<String> filter = new LinkedList<String>();
+		filter.add("*.exe");
+		filter.add("SteamApps");
+		return filter;
 	}
 
+	/**
+	 * Nimmt Suchergebnisse entgegen und legt Executables und SteamApps-Ordner gleich getrennt ab
+	 *
+	 * @param files Suchergebnisse
+	 */
 	@Override
-	public void setFileInputs(List<File> files) throws Exception {
-
+	public void setFileInputs(List<File> files) {
+		exeFiles = new LinkedList<>();
+		for (File currentFile : files) {
+			if (currentFile.isFile() && currentFile.getAbsolutePath().endsWith(".exe")) {
+				exeFiles.add(currentFile);
+			}
+			else if (currentFile.isDirectory() && currentFile.getName() == "SteamApps") {
+				steamAppsFolder = currentFile;
+			}
+			else {
+				throw new RuntimeException("Input passt nicht zu Filter: "
+						+ currentFile.getAbsolutePath());
+			}
+		}
 	}
 
 	@Override
