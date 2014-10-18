d6c6362d948f9c4419285c4f3f70e2021160b6b9
Marvin <MarvinKlose@gmx.net>
2014-10-18 21:47:53 +0200

    sqlite Library sowie Klasse für DB Zugriff
    DataSourceManager kann nun für SQL Statements
    verwendet werden.

---
+++ WHOAMI/src/de/aima13/whoami/support/DataSourceManager.java
@@ -0,0 +1,69 @@
+package de.aima13.whoami.support;
+
+import java.io.File;
+import java.sql.*;
+
+/**
+ * Created by Marvin on 18.10.2014.
+ */
+public class DataSourceManager {
+
+	private Connection dbConnection = null;
+
+	/**
+	 * Konstruktor erzeugt eine Verbindung zur Datenbank und lädt den JDBC Treiber dafür.
+	 * Ebenso gibt es einen Hook, dass wenn die Runtime beendet wird, auf die Connection getrennt
+	 * wird. Falls jemand vergisst die Verbindung zu schließen.
+	 *
+	 * @param sqliteDatabase Dateipfad zur benötigten sqlite Datenbank.
+	 */
+	public DataSourceManager(File sqliteDatabase) {
+		try {
+			Class.forName("org.sqlite.JDBC");
+			dbConnection = DriverManager.getConnection
+					("jdbc:sqlite:" + sqliteDatabase.getAbsolutePath());
+		} catch (ClassNotFoundException e) {
+
+		} catch (SQLException e) {
+
+		}
+		Runtime.getRuntime().addShutdownHook(new Thread() {
+			public void run() {
+				try {
+					if (!dbConnection.isClosed() && dbConnection != null) {
+						dbConnection.close();
+					}
+				} catch (SQLException e) {
+
+				}
+			}
+		});
+
+	}
+
+	/**
+	 * @param statement Query als String die zur Abfrage dienen soll.
+	 * @return ResultSet, dass aus den Ergebnissen der Abfrage besteht.
+	 *
+	 * @throws SQLException Fehler beim Ausführen des SQL Befehls.
+	 */
+	public ResultSet querySqlStatement(String statement) throws SQLException {
+		Statement s = dbConnection.createStatement();
+		ResultSet rs = s.executeQuery(statement);
+		return rs;
+	}
+
+	/**
+	 * Wrapper zum schließen und freigeben der Verbindung!
+	 */
+	public void closeConnection() {
+		try {
+			if (!dbConnection.isClosed() && dbConnection != null) {
+				dbConnection.close();
+			}
+		} catch (SQLException e) {
+
+		}
+	}
+
+}
