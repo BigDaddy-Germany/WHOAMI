a37db2d686fc07ff2696579ce0bd5a29206c878d
Marco Dörfler <marco@doerflers.net>
2014-10-18 19:16:34 +0200

    Methode getTimeProgress - closed #11
    Methode fertig implementiert. Sie gibt einen ganzzahligen Prozent-
    wert zurück, welcher anzeigt, wie viel Prozent der Zeit schon abge-
    laufen ist.

--- WHOAMI/src/de/aima13/whoami/Whoami.java
+++ WHOAMI/src/de/aima13/whoami/Whoami.java
@@ -4,7 +4,22 @@ package de.aima13.whoami;
  * Created by D060469 on 16.10.14.
  */
 public class Whoami {
-	public static void main(String[] args) {
+	private static final int ANALYZE_TIME = 10; // Analysezeit in Sekunden
+	private static long startTime;
 
+	public static void main(String[] args) {
+		startTime = System.nanoTime();
+		
+	}
+
+	public static int getTimeProgress() {
+		float elapsedTime = (float) ((System.nanoTime() - startTime) / 1000000000);
+		int timeProgress = (int) (elapsedTime / ANALYZE_TIME * 100);
+
+		if (timeProgress < 100) {
+			return timeProgress;
+		} else {
+			return 100;
+		}
 	}
 }
