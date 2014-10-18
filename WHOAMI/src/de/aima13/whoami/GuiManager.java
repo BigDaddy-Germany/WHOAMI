b8a1393dc5860f9ce7836b9a2c07b37847f48c5a
Marco Dörfler <marco@doerflers.net>
2014-10-18 17:26:29 +0200

    Testdaten eingefügt um Gui und Module zu testen

--- WHOAMI/src/de/aima13/whoami/GuiManager.java
+++ WHOAMI/src/de/aima13/whoami/GuiManager.java
@@ -4,28 +4,29 @@ package de.aima13.whoami;
  * Created by D060469 on 16.10.14.
  */
 public class GuiManager {
+	final static String GUI_PREFIX = "................";
 	public static void startGui() {
-
+		System.out.println(GUI_PREFIX + "GUI STARTED");
 	}
 
 	public static boolean confirmAgb() {
-
+		System.out.println(GUI_PREFIX + "GUI AGB");
 		return true;
 	}
 
 	public static void showGoodBye() {
-
+		System.out.println(GUI_PREFIX + "GUI GOOD BYE");
 	}
 
 	public static void showProgress() {
-
+		System.out.println(GUI_PREFIX + "GUI PROGRESS STARTED");
 	}
 
 	public static void updateProgress(String status) {
-
+		System.out.println(GUI_PREFIX + "GUI UPDATED: " + status);
 	}
 
 	public static void showReport(String reportHtml) {
-
+		System.out.println(GUI_PREFIX + "GUI REPORT");
 	}
 }
--- WHOAMI/src/de/aima13/whoami/modules/SampleModule.java
+++ WHOAMI/src/de/aima13/whoami/modules/SampleModule.java
@@ -3,38 +3,47 @@ package de.aima13.whoami.modules;
 import de.aima13.whoami.Analyzable;
 
 import java.io.File;
+import java.util.HashMap;
 import java.util.List;
-import java.util.SortedMap;
+import java.util.Map;
 
 /**
  * Created by D060469 on 16.10.14.
  */
 public class SampleModule implements Analyzable {
+	public SampleModule() {
+		System.out.println("Ich wurde konstruiert.");
+	}
+
 	@Override
 	public List<String> getFilter() {
-
+		System.out.println("Nun gebe ich meine Filtereinstellungen aus.");
 		return null;
 	}
 
 	@Override
 	public void setFileInputs(List<File> files) throws Exception {
-
+		System.out.println("Setze Dateien im Testmodul. Folgende Dateien gesetzt:");
+		for (File file : files) {
+			System.out.println(" - " + file.getAbsolutePath());
+		}
 	}
 
 	@Override
 	public String getHtml() {
-
-		return null;
+		return "Hallo. Ich bin das TestModul. Dies ist mein HTML-Text.";
 	}
 
 	@Override
-	public SortedMap<String, String> getCsvContent() {
+	public Map<String, String> getCsvContent() {
+		Map<String, String> csvContent = new HashMap<>();
+		csvContent.put("TestHead", "Test Value");
 
-		return null;
+		return csvContent;
 	}
 
 	@Override
 	public void run() {
-
+		System.out.println("Ich laufe!");
 	}
 }
