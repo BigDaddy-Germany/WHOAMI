6a86cf5570ec4ac5c1c9a9762a2036fdbc922f06
Marco Dörfler <marco@doerflers.net>
2014-10-19 20:10:08 +0200

    Unvollständiger Merge vervollständigt
    Datei wurde beim Mergen vergessen.

--- WHOAMI/src/de/aima13/whoami/Analyzable.java
+++ WHOAMI/src/de/aima13/whoami/Analyzable.java
@@ -10,24 +10,14 @@ import java.util.List;
 public interface Analyzable extends Runnable, Representable {
 
 	/**
-<<<<<<< HEAD
-	 * Liste der Dateien, für welche sich das Modul interessiert
-	 * @return Alle Dateien, welche das Modul benötigt
-=======
-	 * Rückgabe der Filter für Dateien, welche das Modul benötigt
+	 * Filter für Dateien, welche das Modul benötigt
 	 * @return Filtereinstellungen mit * als Platzhalter
->>>>>>> master
 	 */
 	public List<String> getFilter();
 
 	/**
-<<<<<<< HEAD
-	 * Eingabe der für das Modul gefundenen Dateien
-	 * @param files Liste der Dateien
-=======
 	 * Setzen der für das Modul gefundenen Dateien
 	 * @param files Liste der gefundenen Dateien
->>>>>>> master
 	 * @throws Exception Ein Fehler ist aufgetreten
 	 */
 	public void setFileInputs(List<File> files) throws Exception;
