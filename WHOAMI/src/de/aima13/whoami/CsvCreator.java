6547592c5589d3f9690583d0b1dca11a89ae1765
Marco Dörfler <marco@doerflers.net>
2014-10-20 20:48:12 +0200

    CSV Map aus allem Modulen zusammengestellt
    ...und alte Lib gelöscht. Neue muss her.

--- WHOAMI/src/de/aima13/whoami/CsvCreator.java
+++ WHOAMI/src/de/aima13/whoami/CsvCreator.java
@@ -1,6 +1,8 @@
 package de.aima13.whoami;
 
+import java.io.File;
 import java.util.List;
+import java.util.Map;
 import java.util.SortedMap;
 import java.util.TreeMap;
 
@@ -12,7 +14,9 @@ public class CsvCreator {
 	private static enum CSV_STATUS {
 		IS_PERFECT, WRONG_FORMAT, DOESNT_EXIST
 	}
-	private static final String FILE_NAME = "WHOAMI_Analyze_Results.csv";
+	private static final String FILE_NAME = "WHOAMI_Analyze_Results.csv"; // Name der CSV Datei
+	private static final File csvDatei = new File(FILE_NAME); // File Datei der CSV Datei
+	private static final String PREFIX_SEPERATOR = "_"; // Seperator zw. Modulname und Header
 
 	/**
 	 * Starten der Speicherung
@@ -20,16 +24,37 @@ public class CsvCreator {
 	 * @throws Exception Ein Fehler ist aufgetreten
 	 */
 	public static void saveCsv(List<Representable> representables) throws Exception {
-		SortedMap<String, String> csvValues = new TreeMap<>();
+		SortedMap<String, String> completeCsvContent = new TreeMap<>();
+
+		System.out.println("\n\nStarting CsvCreator\n----------------\n");
 
 		// CSV Werte aus allen Representables ziehen
 		for (Representable representable : representables) {
-			csvValues.putAll(representable.getCsvContent());
+			SortedMap<String, String> moduleCsvContent = representable.getCsvContent();
+
+			if (moduleCsvContent != null) {
+				// Header werden mit Prefix versehen -> keine Namensgleichheit
+				String prefix = representable.getClass().getSimpleName();
+
+				for (Map.Entry<String, String> moduleCsvCol : moduleCsvContent.entrySet()) {
+					// Titel mit Prefix versehen und Spalte hinzufügen
+					completeCsvContent.put(prefix + PREFIX_SEPERATOR + moduleCsvCol.getKey(),
+							moduleCsvCol.getValue());
+
+					System.out.println("Found Pair: (" + prefix + PREFIX_SEPERATOR +
+							moduleCsvCol.getKey() + "|" +
+							moduleCsvCol.getValue() + ")");
+				}
+			}
 		}
+
+		System.out.println((new File(".")).getAbsolutePath());
+
+		getCsvStatus(completeCsvContent);
 
 	}
 
-	private static CSV_STATUS getCsvStatus(SortedMap<String, String> representables) {
+	private static CSV_STATUS getCsvStatus(SortedMap<String, String> moduleCsvContent) {
 		
 		return CSV_STATUS.IS_PERFECT;
 	}
