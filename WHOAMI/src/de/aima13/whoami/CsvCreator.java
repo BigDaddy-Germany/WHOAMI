0eedd41c0ff3d9a7d284134103c6ff332e589e3e
Marco Dörfler <marco@doerflers.net>
2014-10-20 21:17:06 +0200

    Methode getCsvStatus fertiggestellt

--- WHOAMI/src/de/aima13/whoami/CsvCreator.java
+++ WHOAMI/src/de/aima13/whoami/CsvCreator.java
@@ -1,6 +1,11 @@
 package de.aima13.whoami;
 
+import au.com.bytecode.opencsv.CSVReader;
+
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 import java.util.SortedMap;
@@ -17,6 +22,9 @@ public class CsvCreator {
 	private static final String FILE_NAME = "WHOAMI_Analyze_Results.csv"; // Name der CSV Datei
 	private static final File csvDatei = new File(FILE_NAME); // File Datei der CSV Datei
 	private static final String PREFIX_SEPERATOR = "_"; // Seperator zw. Modulname und Header
+
+	private static final char CSV_SEPERATOR = ';';
+	private static final char CSV_QUOTECHAR = '"';
 
 	/**
 	 * Starten der Speicherung
@@ -50,12 +58,56 @@ public class CsvCreator {
 
 		System.out.println((new File(".")).getAbsolutePath());
 
-		getCsvStatus(completeCsvContent);
+		// Header als String Array exportieren
+		CSV_STATUS csvStatus = getCsvStatus(
+				completeCsvContent
+						.keySet()
+						.toArray(
+								new String[
+										completeCsvContent
+												.keySet()
+												.size()
+										]
+						)
+		);
+
+		System.out.println(csvStatus);
 
 	}
 
-	private static CSV_STATUS getCsvStatus(SortedMap<String, String> moduleCsvContent) {
-		
-		return CSV_STATUS.IS_PERFECT;
+	private static CSV_STATUS getCsvStatus(String[] moduleHeader) {
+		try {
+			CSVReader reader = new CSVReader(new FileReader(csvDatei), CSV_SEPERATOR, CSV_QUOTECHAR);
+
+			// Wir gehen davon aus, dass die erste Zeile der Header ist
+			String[] header = reader.readNext();
+			reader.close();
+
+			// zu Anfang gehen wir von einem korrekten Header aus, wenn die header die selbe
+			// Größe haben
+			boolean headerCorrect = (header.length == moduleHeader.length);
+
+			// Jetzt werden alle Spalten geprüft, sofern die Anzahl die selbe ist
+			if (headerCorrect) {
+				for (int i = 0; i < header.length; i++) {
+					if (!header[i].equals(moduleHeader[i])) {
+						headerCorrect = false;
+						break;
+					}
+				}
+			}
+
+			// entsprechende Rückgabe
+			if (headerCorrect) {
+				return CSV_STATUS.IS_PERFECT;
+			} else {
+				return CSV_STATUS.WRONG_FORMAT;
+			}
+
+
+		} catch (IOException e) {
+			// Hier landen wir, wenn die Datei nicht existiert
+			return CSV_STATUS.DOESNT_EXIST;
+		}
 	}
 }
