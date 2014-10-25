c7f6d4d30c3a4e1242d4c1cf2e7f13407d21e783
Marco Dörfler <marco@doerflers.net>
2014-10-25 12:43:35 +0200

    ReportCreator angefangen
    HTML Datei in Resourcen erstellt.

--- WHOAMI/src/de/aima13/whoami/ReportCreator.java
+++ WHOAMI/src/de/aima13/whoami/ReportCreator.java
@@ -1,13 +1,16 @@
 package de.aima13.whoami;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by D060469 on 16.10.14.
  */
 public class ReportCreator {
-	public ReportCreator(List<Representable> representables) {
+	private List<Representable> representableList;
 
+	public ReportCreator(List<Representable> representables) {
+		this.representableList = representables;
 	}
 
 	public void savePdf() throws Exception {
@@ -17,5 +20,15 @@ public class ReportCreator {
 	public String getHtml() {
 
 		return null;
+	}
+
+	private List<String> getHtmlSnippets() {
+		List<String> htmlSnippets = new ArrayList<>();
+
+		for (Representable representable : this.representableList) {
+			htmlSnippets.add(representable.getHtml());
+		}
+
+		return htmlSnippets;
 	}
 }
