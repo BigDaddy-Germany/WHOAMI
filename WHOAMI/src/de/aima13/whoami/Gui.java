210eead30f09fe63909ff8fca7809b629fc5224b
marvinklose <MarvinKlose@gmx.net>
2014-10-28 22:48:33 +0100

    Ergebnisscreen als FXML Closes #62
    +Code sortiert

--- WHOAMI/src/de/aima13/whoami/Gui.java
+++ WHOAMI/src/de/aima13/whoami/Gui.java
@@ -5,17 +5,14 @@ import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 import javafx.stage.WindowEvent;
 
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -25,14 +22,34 @@ import java.io.InputStream;
 public class Gui extends Application {
 	private static boolean eulaConfirmed = false;
 	private GuiController guiController;
-	private javafx.scene.web.WebEngine webEngine;
+	private WebEngine webEngine;
+
+	/**
+	 * @return Ob die EULA akzeptiert wurde oder nicht.
+	 */
+	public static boolean eulaConfirmed() {
+		return eulaConfirmed;
+	}
+
+	/**
+	 * Methode startet den FX Application Thread
+	 */
+	public static void showEulaGui() {
+		try {
+			launch();
+		} catch (Exception e) {
+			//Hm dann ist wohl was beim start() kaputt
+			System.exit(4711);
+		}
+	}
 
 	/**
 	 * Diese Methode wird vom launch aufgerufen und bildet somit den Start des FX Application
 	 * Threads. Zuerst werden die 3. Stages konstruiert und initialisiert. Die 2. und 3. sind
 	 * final weil sie aus anonymen Listenern aufgerufen werden.
+	 *
 	 * @param primaryStage Wird vom launch() automatisch gefüllt. Ist die Stage die als erstes
-	 *                        live geht.
+	 *                     live geht.
 	 * @throws Exception JavaFX Exceptions oder IO Exceptions
 	 */
 	@Override
@@ -44,12 +61,12 @@ public class Gui extends Application {
 		first.setOnHiding(new EventHandler<WindowEvent>() {
 			public void handle(WindowEvent windowEvent) {
 				eulaConfirmed = guiController.isEulaAccepted();
-				if(!eulaConfirmed){
+				if (!eulaConfirmed) {
 					System.exit(0);
 				}
-					Thread t = new Thread(new Whoami());
-					t.start();
-					second.show();
+				Thread t = new Thread(new Whoami());
+				t.start();
+				second.show();
 
 			}
 		});
@@ -67,8 +84,10 @@ public class Gui extends Application {
 	/**
 	 * Erster Teil des Programms mit der EULA und den jeweiligen Checkboxen.
 	 * Erst wenn die Stage aus dem Fokus verschwindet läuft WHOAMI im Thread los.
+	 *
 	 * @param primaryStage Eingangsparameter der start Methode die vom FX Thread kommt
 	 * @return Stage    Erster Startbildschrim
+	 *
 	 * @throws IOException Wenn FXML Datei nicht gefunden wurde.
 	 */
 	private Stage initFirstStage(final Stage primaryStage) throws IOException {
@@ -93,7 +112,9 @@ public class Gui extends Application {
 	 * Stage besteht im wesentlichen aus einer TextArea auf der die Kommentare anzeigt werden,
 	 * die von den Modulen oder sonst wem kommen. Die ProgressBar läuft unten munter von links
 	 * nach Rechts und wieder zurück.
+	 *
 	 * @return Stage 2. Screen der den Fortschritt anzeigt.
+	 *
 	 * @throws IOException Wenn die passende FXML Datei nicht gefunden wird.
 	 */
 	private Stage initSecondStage() throws IOException {
@@ -118,50 +139,29 @@ public class Gui extends Application {
 				event.consume();
 			}
 		});
-		return  secondaryStage;
+		return secondaryStage;
 	}
 
 	/**
-	 * Programmatische Erzeugung der 3. und letzten Stage. Hier wird eine Stage erzeugt die einen
-	 * Webview und eine BrowserEngine. Das ganze befindet sich im Center eines BroderPanes. Hier
-	 * ist es möglich weitere Steuerelement an den andere Positionen zu platzieren.
+	 * Fenster mit dem Titel dein Ergebnis. In dieser Stage wird das Ergebnis präsentiert. Der
+	 * Stagestyle ist nicht mehr der reduzierte, sondern es ist wieder alles in vollem Umfang
+	 * einsehbar. Die FXML beschreibt den Screen, der lediglich aus einer WebView besteht.
+	 * Die Engine wird lokal gespeichert. Der CSS Style wird ebenfalls wiederverwendet.
+	 *
 	 * @return Stage Dritte und letzte Stage
 	 */
-	private Stage initThirdStage() {
+	private Stage initThirdStage() throws IOException {
+		final FXMLLoader thirdLoader = new FXMLLoader();
 		final Stage thirdStage = new Stage();
 		thirdStage.setTitle("Dein Ergebnis");
-		thirdStage.setWidth(1024);
-		thirdStage.setHeight(768);
-		Scene scene = new Scene(new Group());
 
-		BorderPane rootBox = new BorderPane();
-		final WebView browser = new WebView();
-		webEngine = browser.getEngine();
-
-		rootBox.setCenter(browser);
-		scene.setRoot(rootBox);
-
+		InputStream isThird = Whoami.class.getResourceAsStream("/reportScreen.fxml");
+		Parent rootReport = (Parent) thirdLoader.load(isThird);
+		webEngine = ((WebView) rootReport.lookup("#webView")).getEngine();
+		Scene scene = new Scene(rootReport);
+		scene.getStylesheets().addAll(Whoami.class.getResource("/gui/window.css")
+				.toExternalForm());
 		thirdStage.setScene(scene);
 		return thirdStage;
-	}
-
-
-	/**
-	 *
-	 * @return Ob die EULA akzeptiert wurde oder nicht.
-	 */
-	public static boolean eulaConfirmed() {
-		return eulaConfirmed;
-	}
-
-	/**
-	 * Methode startet den FX Application Thread
-	 */
-	public static void showEulaGui() {
-		try {
-			launch();
-		}catch (Exception e){
-			//Hm dann ist wohl was beim start() kaputt
-		}
 	}
 }
