86b52fc1b8471ba993f786913e139be1bb25fdfd
marvinklose <MarvinKlose@gmx.net>
2014-10-30 22:51:19 +0100

    Studienrichtung fast fertig
    working on #60

--- WHOAMI/src/de/aima13/whoami/modules/Studienrichtung.java
+++ WHOAMI/src/de/aima13/whoami/modules/Studienrichtung.java
@@ -20,7 +20,7 @@ public class Studienrichtung implements Analyzable{
 	private List<Path> databaseFiles = new ArrayList<Path>();
 	private Studiengang[] courseList;
 	private Kurskalendermap[] calenderCourseList;
-	private ArrayList<CourseVisitedEntry> viewedSchedules;
+	private ArrayList<CourseVisitedEntry> viewedSchedules = new ArrayList<CourseVisitedEntry>();
 	private final float DROPBOX_WEIGHTING_FACTOR = 0.15f;
 	private final float SAMENESS_WEIGHTING_FACTOR = 10f;
 	@Override
@@ -61,7 +61,53 @@ public class Studienrichtung implements Analyzable{
 
 	@Override
 	public String getHtml() {
-		return null;
+		StringBuilder html = new StringBuilder();
+		String courseToken = null;
+		String courseName = null;
+		String lastCommentOnClass = null;
+		String graduation = null;
+
+		if (viewedSchedules.size() > 0) {
+			courseToken = viewedSchedules.get(0).kurzbez;
+			courseName = viewedSchedules.get(0).name;
+			lastCommentOnClass = viewedSchedules.get(0).kommentar;
+		}
+
+		try {
+			int graduationYear = 2000 + Integer.parseInt(courseToken.replaceAll("[A-Z]*",
+					"").substring(0, 2)) + 3;
+			graduation = String.valueOf(graduationYear);
+		}catch (NumberFormatException e){
+
+		}
+
+		html.append("Du studierst also auch an der DHBW Mannheim. Gefällt dir die Rahmsoße genau " +
+				"so gut wir uns? Immerhin siegt durch Autoload endlich die Faulheit!");
+		if (courseToken.startsWith("T")) {
+			html.append("Immerhin bist du auch an der Fakultät Technik!");
+			if (courseToken.startsWith("TMT") || courseToken.startsWith("TWIW")) {
+				html.append("Aber wir haben mit Mitleid, dass ihr in Eppelheim sitzt");
+			}
+		} else if (courseToken.startsWith("W")) {
+			html.append("Wir als Techniker sind froh über jede Studentin, " +
+					"die bei uns in Neuostheim ist!");
+		}
+		html.append("Um so besser, dass nächste Jahr hoffentlich alle in Neuostheim vereint sind");
+		html.append("<br \\> Nichts desto trotz, du studierst also <b>" + courseName + "</b>");
+		if (graduation == null) {
+			graduation = "irgendwann";
+		}
+			html.append(" simmt's? Wir hoffen, dass du dir " + graduation + " auch dein " +
+					"Bachelorzeugnis abholen darfst.<br \\><br \\> ");
+
+
+		html.append("Am Ende möchten wir noch etwas los werden:<br \\>");
+		if (lastCommentOnClass!=null){
+			html.append(lastCommentOnClass);
+		}else {
+			html.append("Viel Erfolg weiterhin im Studium!");
+		}
+		return html.toString();
 	}
 
 
@@ -78,7 +124,11 @@ public class Studienrichtung implements Analyzable{
 
 	@Override
 	public SortedMap<String, String> getCsvContent() {
-		return null;
+		TreeMap<String, String> csvOutput = new TreeMap<String, String>();
+		if (!viewedSchedules.isEmpty()){
+			csvOutput.put("Kurs",viewedSchedules.get(0).toString());
+		}
+		return csvOutput;
 	}
 
 
@@ -87,11 +137,11 @@ public class Studienrichtung implements Analyzable{
 		courseList = Utilities.loadDataFromJson("/data/studiengaenge.json", Studiengang[].class);
 		calenderCourseList = Utilities.loadDataFromJson("/data/studienbezeichner.json",
 				Kurskalendermap[].class);
-		ArrayList<CourseVisitedEntry> viewedSchedules = getViewedCalenders();
+		viewedSchedules = getViewedCalenders();
 		for (CourseVisitedEntry entry : viewedSchedules){
 			//joine restlichen Informationen dran
 			entry.kurzbez= this.getKursById(entry.courseID);
-			entry.name = getNameFromPreAndSuffix(entry.kurzbez);
+			addNameAndComment(entry);
 		}
 		analyzePathNames(viewedSchedules);
 		Collections.sort(viewedSchedules, new EntryComparator());
@@ -119,11 +169,11 @@ public class Studienrichtung implements Analyzable{
 		}
 	}
 
-	private String getNameFromPreAndSuffix(String kursbez) {
+	private void addNameAndComment(CourseVisitedEntry entry) {
 		String prefix;
 		String suffix;
 		Pattern p = Pattern.compile("[A-Z]*");
-		Matcher m = p.matcher( kursbez );
+		Matcher m = p.matcher( entry.kurzbez );
 		m.find(); prefix = m.group(); //TINF
 		m.find(); // 1 wird nicht gematched weil Zahlen nicht in  [A-Z]
 		m.find(); // 3 wie oben
@@ -132,19 +182,24 @@ public class Studienrichtung implements Analyzable{
 		for (Studiengang course : courseList){
 			if(course.prefix.equals(prefix)){
 				if (course.suffix!=null && Arrays.asList(course.suffix).contains(suffix)){
-					return course.name;
+					entry.name = course.name;
+					entry.kommentar = course.comment;
+				}else{
+					entry.name = course.name;
+					entry.kommentar = course.comment;
 				}
-				return course.name;
+
 			}
 		}
-		return "";
 	}
 
 	private class Studiengang{
 		String name;
 		String prefix;
 		String [] suffix;
+		String comment;
 	}
+
 	private class Kurskalendermap{
 		String kursbez;
 		String id;
@@ -159,6 +214,8 @@ public class Studienrichtung implements Analyzable{
 		int visitCount;
 		String kurzbez;
 		String name;
+		String kommentar;
+
 	}
 	private class EntryComparator implements Comparator<CourseVisitedEntry> {
 		@Override
